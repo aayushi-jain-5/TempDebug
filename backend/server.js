74b33a35b4a1f8b0ccd4e3b231df1ee43d413ae3
Aayushi Jain <101443827+aayushi-jain-5@users.noreply.github.com>
2024-05-18 16:03:21 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config'
+// import ordrRouter from './routes/orderRoute.js';
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js';
 
 
 // app config
@@ -19,13 +22,17 @@ app.use(cors())
 // db cononection
 connectDB();
 
-// api endpint
+// api endpoint
 app.use("/api/food",foodRouter);
 app.use("/images",express.static("uploads"));
 app.use("/api/user",userRouter);
+app.use("/api/cart",cartRouter);
+app.use("/api/order",orderRouter);
+
+
 
 app.get("/",(req,res)=>{
-    res.send("API workign man")
+    res.send("API working man")
 })
 
 
